717a365a9571079bfebf334f68e8c0e6e0e11c0e
Nick <nwharr12@gmail.com>
2021-09-09 14:47:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let friends = ['Nick', 'Garrett', 'Nathan', 'Joseph', 'Brayden'];
+
+//for(Starting point; stopping point; up[++] or down[--])
+for (let i = 0; i < friends.length; i++) {
+
+    //nested for loop   
+    for (let u = 99; u > 0; u--) {
+        if (u == 2) {
+            console.log(`${u} lines of code in the file, ${u} lines of code. ${friends[i]} takes one out,
+        debugs it out. ${u - 1} line of code in the file!`)
+        }
+
+        else if (u == 1) {
+            console.log(`${u} line of code in the file, ${u} line of code. ${friends[i]} takes one out,
+            debugs it out. ${u - 1} lines of code in the file!`)
+        }
+
+        else {
+            console.log(`${u} lines of code in the file, ${u} lines of code. ${friends[i]} takes one out,
+    debugs it out. ${u - 1} lines of code in the file!`)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
